51ad6cddafd32afe8b4bdd99ae20718bbd4c1556
Michael Eco <michael.gunawan2002@gmail.com>
2024-08-21 16:08:31 +0800

    Add sum function

--- src/challenge_1_frontend/src/App.jsx
+++ src/challenge_1_frontend/src/App.jsx
@@ -2,15 +2,26 @@ import { useState } from 'react';
 import { challenge_1_backend } from 'declarations/challenge_1_backend';
 
 function App() {
-  const [greeting, setGreeting] = useState('');
+  // const [greeting, setGreeting] = useState('');
+  const [result, setResult] = useState("");
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    challenge_1_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
+  // function handleSubmit(event) {
+  //   event.preventDefault();
+  //   const name = event.target.elements.name.value;
+  //   challenge_1_backend.greet(name).then((greeting) => {
+  //     setGreeting(greeting);
+  //   });
+  //   return false;
+  // }
+
+  function handleSum(e) {
+    e.preventDefault();
+    const a = parseInt(e.target.elements.a.value);
+    const b = parseInt(e.target.elements.b.value);
+    challenge_1_backend.sum(a, b).then((_result) => {
+      console.log(_result)
+      setResult(_result.toString());
     });
-    return false;
   }
 
   return (
@@ -18,12 +29,16 @@ function App() {
       <img src="/logo2.svg" alt="DFINITY logo" />
       <br />
       <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
+
+      <h1>Challenge 1: Simple Calculator</h1>
+      <form action="#" onSubmit={handleSum}>
+        <label htmlFor="a">Number 1</label>
+        <input type="number" id="a" name="a" />
+        <label htmlFor="b">Number 1</label>
+        <input type="number" id="b" name="b" />
+        <button type="submit">Calculate</button>
       </form>
-      <section id="greeting">{greeting}</section>
+      <section id="result">Result: {result}</section>
     </main>
   );
 }
